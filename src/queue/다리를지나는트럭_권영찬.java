30d89a1ddfc89c00e4be95d7e42f644e55feb826
zave7 <zave7@naver.com>
2021-02-18 18:10:48 +0900

    다리를지나는트럭 완료

--- src/queue/다리를지나는트럭_권영찬.java
+++ src/queue/다리를지나는트럭_권영찬.java
@@ -1,128 +1,63 @@
 package queue;
 
 public class 다리를지나는트럭_권영찬 {
-    public static void main(String[] args) {
-
-//        2	10	[7,4,5,6]	8
-//        100	100	[10]	101
-//        100	100	[10,10,10,10,10,10,10,10,10,10]	110
-
-        int[] bridge_lengths = new int[]{2, 100, 100};
-        int[] weights = new int[]{10, 100, 100};
-        int[] truck_weights1 = new int[]{7,4,5,6};
-        int[] truck_weights2 = new int[]{10};
-        int[] truck_weights3 = new int[]{10,10,10,10,10,10,10,10,10,10};
-
-
-        int bridge_length = bridge_lengths[2];
-        int weight = weights[2];
-        int[] truck_weights = truck_weights3;
-
-        int totalOfTrucks = truck_weights.length;
-        int numberOfTrucks = totalOfTrucks;
-        BridgeQueue queue = new BridgeQueue(bridge_length, weight, truck_weights);
-
-//        int second = 0;
-//        while(numberOfTrucks > 0) {
-//            if(queue.add(truck_weights[totalOfTrucks-numberOfTrucks])) {
-//                numberOfTrucks--;
-//            }
-//            second++;
-//        }
-
-        System.out.println(queue.execute());
-    }
-
 }
 
 class BridgeQueue {
 
-    // 가장중요한 포인트 : 트럭들은 항상 붙어서 지나감 (트럭 사이의 공간이 없음)
-
-    int[] queue; // 배열
-    int maxLocation; // 다리 길이
-    int location; // 가장 앞선 트럭의 위치
-    int maxWeight; // 최대 무게
-    int totalWeight; // 현재 무게
-    int next; // 다음 삽입 위치
-
     int[] truckWeights;
+    int[] queue;
+    int[] sequence;
     int totalOfTrucks;
-    int numberOfTrucks;
+    int maxLocation;
+    int location;
+    int maxWeight;
+    int totalWeight;
+    int next;
+    int idxOfVeryFrontTruck;
+    int time;
 
-    int totalCountTrucks;
-
-    public BridgeQueue(int bridgeLength, int maxWeight, int[] truckWeights) {
-        this.queue = new int[bridgeLength];
+    public BridgeQueue(int[] truckWeights, int bridgeLength, int maxWeight) {
+        this.truckWeights = truckWeights;
+        this.queue = new int[truckWeights.length];
+        this.sequence = new int[truckWeights.length];
         this.maxLocation = bridgeLength;
         this.maxWeight = maxWeight;
+        this.totalOfTrucks = truckWeights.length;
         this.location = 0;
         this.totalWeight = 0;
         this.next = 0;
-
-        this.truckWeights = truckWeights;
-        this.totalOfTrucks = this.truckWeights.length;
-        this.numberOfTrucks = totalOfTrucks;
-        this.totalCountTrucks = totalOfTrucks;
+        this.idxOfVeryFrontTruck = 0;
+        this.time = 0;
     }
-
     public int execute() {
-        int second = 0;
-        while(totalCountTrucks > 0) {
-            if(numberOfTrucks > 0) {
-                if(add(truckWeights[totalOfTrucks-numberOfTrucks])) {
-                    numberOfTrucks--;
-                }
-            } else {
-                run();
-            }
-            second++;
+        while(idxOfVeryFrontTruck < totalOfTrucks) {
+            if(next < totalOfTrucks) add(truckWeights[next]);
+            else moveOfRemainTrucks();
         }
-        return second;
+        return time;
     }
-
-    public void run() {
-        if(location == maxLocation) {
-            remove();
-            totalCountTrucks--;
-        }
-        if(location < maxLocation) {
-            location++;
-        }
+    public void moveOfRemainTrucks() {
+        minusPassingTruck();
+        moveForward();
     }
-
-    // 삽입(배열의 맨뒤)
-    public boolean add(int truck) {
-
-        if(location == maxLocation) { // 가장 앞선 트럭의 위치가 마지막 위치일 경우 해당 트럭 remove
-            remove();
-            totalCountTrucks--;
-        }
-
-        if((totalWeight+truck) > maxWeight) { // 현재 무게 + 들어올 트럭의 무게가 최대 무게 이하일때 add
-            if(location < maxLocation) {
-                location++;
-            }
-            return false;
-        }
-
-        this.queue[next] = truck;
-        totalWeight += truck; // 무게 추가
-        next++;
-        location++;
-        return true;
+    public void add(int truck) {
+        minusPassingTruck();
+        moveForward();
+        if((totalWeight+truck) > maxWeight) return;
+        queue[next] = truck;
+        sequence[next++] = time;
+        totalWeight += truck;
     }
-
-    // 삭제(배열의 맨앞)
     public void remove() {
-        totalWeight -= this.queue[0];
-        for(int i=1; i<next; i++) {
-            this.queue[i-1] = this.queue[i];
-        }
-        next--;
-        if(next == 0) { //remove 후 next 가 0일 경우 location 0으로 초기화
-            location = 0;
-        }
+        if((totalWeight -= queue[idxOfVeryFrontTruck++]) == 0) location = 0;
+        else location += sequence[idxOfVeryFrontTruck-1] - sequence[idxOfVeryFrontTruck];
     }
-
+    private void minusPassingTruck() {
+        if (location == maxLocation) remove();
+    }
+    private void moveForward() {
+        if (location < maxLocation) location++;
+        time++;
+    }
 }
