e978f98e1cb076c6101caffc7ffc6974559881e0
KimJinYoung <zsktnflahs13@gmail.com>
2022-04-14 12:30:55 +0900

    Lv1 Code Test Before Refacktoring

---
+++ src/Lv1/로또의_최고_순위와_최저_순위/JY.java
@@ -0,0 +1,28 @@
+package 로또_최고순위_최저순위;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JY {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int currentCount =0;
+        List aimNumber = new ArrayList();
+        // 배열안 0갯수 파악
+        currentCount= (int) Arrays.stream(lottos).filter(fi->fi==0).count();
+        // lotto배열과 win 배열중 일치하는것을 aim리스트에 넣음
+        Arrays.stream(win_nums).forEach(x -> {if(Arrays.stream(lottos).filter(fi -> fi==x).count()>0) {aimNumber.add(x);}});
+        int maxCurrent;
+        int minCurrent;
+        // current => 0~6 맞는갯수만큼
+        // aim -> 0~6맞는갯수만큼
+        // 6개중 1개맞았을때 0개맞았을때를 꼴지로 계산하기 때문에 이렇게처리함
+        maxCurrent = 7-(currentCount+aimNumber.size());
+        minCurrent = 7-aimNumber.size();
+        answer[0] = maxCurrent<7 ? maxCurrent : 6;
+        answer[1] = minCurrent<7 ? minCurrent : 6;
+
+        return answer;
+    }
+}
